c290ad2c4266cfa039cee991e2e344971074befa
ViktorSchmidt <vitosvitosvitos31@gmail.com>
2023-11-30 22:17:56 +0300

    inittial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Задание 1");
+//        int year = 2100;
+//            if (year % 4 == 0 && year % 400 == 0 || year % 100 != 0) {
+//                System.out.println(year + " год является високосным");
+//            } else {
+//                System.out.println(year + " год не является високосным");
+//            }
+
+    }
+}
